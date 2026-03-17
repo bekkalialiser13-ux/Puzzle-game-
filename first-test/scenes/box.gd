5fa7847196b6f35bd1870b5ec184ea2b3a292eb5
bekkalialiser13-ux <bekkalialiser13@gmail.com>
2026-03-17 12:04:18 +0500

    prototype 0.2

--- first-test/scenes/box.gd
+++ first-test/scenes/box.gd
@@ -10,6 +10,14 @@ func push(direction: Vector2):
 	is_moving = true
 	var target_position = position + direction * grid_size
 	
+	# Пытаемся двинуться
+	var collision = move_and_collide(direction * grid_size, true)
+	
+	if collision:
+		# Столкновение - не двигаемся
+		is_moving = false
+		return
+	
 	# Анимация движения
 	var tween = create_tween()
 	tween.tween_property(self, "position", target_position, 0.2)
---
+++ first-test/scenes/menu.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("Play"):
+		get_tree().change_scene_to_file("res://scenes/world.tscn")
+	if Input.is_action_pressed("Exit"):
+		get_tree().quit()
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
